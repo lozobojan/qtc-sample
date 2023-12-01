32d04c31f15fdf87f155952f09fef856cb59d6e4
Bojan Lozo <bojanlo@maestralsolutions.com>
2023-12-01 22:59:06 +0100

    Added a register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,16 +33,23 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <x-button class="ms-4">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+
+            <div class="flex items-center justify-between mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 float-left" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
             </div>
+
+
         </form>
     </x-authentication-card>
 </x-guest-layout>
